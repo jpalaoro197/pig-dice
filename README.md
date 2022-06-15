2a39507f935058236b03fdb9929b3d39297938c3
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-15 15:10:22 -0700

    add buisness logice incl. getInt function, addScore function, addTotalScore function, basic html page with roll buttons and space for results to output, add ui logic to connect buisness logic to html.
    Co-authored-by: Tyler Emmerson <tyleremmerson0216@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-description: player
+
+# _Pig-Dice_
+
+#### By _**Jacob Palaoro & Tyler Emmerson**_
+
+#### _Description_
+
+This is a Pig Dice game with options for playing with one die or two dice. Players take turns rolling a die or dice. They can decide to continue rolling to acrue potential points or "hold" and add those potential points to their score. Certain roll combinations, however, may cause a turn to end and all potential points to be lost.
+
+*_Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to "hold":_
+*_If the player rolls a 1, they score nothing and it becomes the next player's turn._
+*_If the player rolls any other number, it is added to their turn total and the player's turn continues._
+*_If a player chooses to "hold", their turn total is added to their score, and it becomes the next player's turn. The first player to score 100 or more points wins._
+
+## Technologies Used
+
+* _CSS_
+* _Javascript_
+* _HTML_
+* _jQuery_
+* _Bootstrap_
+* _Git_
+* _Markdown_
+* _GitHub_
+
+## Setup/Installation Requirements
+
+1. Clone this repository to your desktop.
+2. Navigate to the top level of directory.
+3. Open *index.html* in your browser.
+
+## Known Bugs
+
+* __
+
+## License
+
+_<p><a href="LICENSE.txt">MIT</a></p>_
+
+Copyright (c) 2022 Jacob Palaoro & Tyler Emmerson(s)_
+
+## Tests
+
+Test 1
+Describe: create a random 1-6 number generator
+Code: 
+ 
+
+
+
+Test 2
+Describe: create player object
+Code: 
+ 
+
+
+Test 3 
+Describe: 
+Code:
+
+Test 3 
+Describe: 
+Code:
+
+ 
+
+
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,70 @@
-function player(name, score) {
+// Business Logic
+function player(name, score, roll) {
   this.name = name;
-  this.overallScore = 0;
-  this.turnTotal = 0;
+  this.score = 0;
+  this.roll = 0;
+  this.totalScore = 0;
 }
 
-let player1 = new player("player 1");
-let player2 = new player("player 2");
+let player1 = new player("jacob", 0, 0);
+let player2 = new player("tyler", 0, 0);
 
 
 
-let diceRoll = function() {
-  let roll = math.ceil((math.random() * 6) + 1);
-return roll;
+player.prototype.getInt = function(max) {
+  this.roll = Math.ceil(Math.random() * 6);
 };
 
-function diceRoll() {
-  return Math.floor(Math.random() *6)
+player.prototype.addScore = function() {
+  if (this.roll > 1) {
+    this.score += this.roll;
+  } 
+  else if (this.roll = 1) {
+    this.score = 0;
 }
+}
+player.prototype.addTotalScore = function() {
+  this.totalScore += this.score;
+  this.score = 0;
+}
+
+// UI logic
+$(document).ready(function() {
+  $("#button1").click(function(event) {
+    event.preventDefault();
+    player1.getInt();
+    player1.addScore();
+    $("#p1score").html(player1.score);
+    $("#p1lastRoll").html(player1.roll);
+    console.log(player1);
+  });
+});
+
+$(document).ready(function() {
+  $("#button2").click(function(event) {
+    event.preventDefault();
+    player2.getInt();
+    player2.addScore();
+    $("#p2score").html(player2.score);
+    $("#p2lastRoll").html(player2.roll);
+    console.log(player1);
+
+  });
+});
+
+$(document).ready(function() {
+  $("#p1hold").click(function(event) {
+    event.preventDefault();
+    player1.addTotalScore();
+    $("#p1totalScore").html(player1.totalScore)
+  })
+})
+
+$(document).ready(function() {
+  $("#p2hold").click(function(event) {
+    event.preventDefault();
+    player2.addTotalScore();
+    $("#p2totalScore").html(player2.totalScore)
+  })
+})
+
