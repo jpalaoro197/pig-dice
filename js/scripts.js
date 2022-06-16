7ce7dd44ceb5f3aea75ab0e707b1592b9d43555d
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-16 10:53:50 -0700

    add winner function that alerts a player if they have held  with the total score of 100 or more.
    Co-authored-by: Tyler Emmerson <tyleremmerson0216@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,6 @@ player.prototype.addScore = function () {
   else if (this.roll = 1) {
     this.score = 0;
     alert("turnover fool");
-    show
   }
 };
 
@@ -31,12 +30,19 @@ player.prototype.addTotalScore = function() {
   this.score = 0;
 };
 
+player.prototype.winner = function() {
+  if (this.totalScore >= 100) {
+    alert("player  wins");
+  };
+};
+
 // UI logic
 $(document).ready(function() {
   $("#button1").click(function(event) {
     event.preventDefault();
     player1.getInt();
     player1.addScore();
+    
     $("#player1Score").html(player1.score);
     $("#player1LastRoll").html(player1.roll);
     console.log(player1);
@@ -60,6 +66,7 @@ $(document).ready(function() {
     event.preventDefault();
     player1.addTotalScore();
     $("#player1TotalScore").html(player1.totalScore)
+    player1.winner();
   });
 });
 
@@ -68,14 +75,21 @@ $(document).ready(function() {
     event.preventDefault();
     player2.addTotalScore();
     $("#player2TotalScore").html(player2.totalScore)
+    player2.winner();
+  });
+});
 
+$(document).ready(function() {
+  $("button#player1Hold").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
   });
 });
 
 $(document).ready(function() {
   $("button#player2Hold").click(function() {
-    $("column2").removeClass();
-    $("column2").addClass("green-background");
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
 
